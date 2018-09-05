9bdc9ea030202f7527ab1716837328ffd0ee12fd
Jennifer Nordell <jennifer.nordell@outlook.com>
2018-09-05 20:18:39 +0200

    addition of two routes

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const app = express();
 app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
 app.listen(3000, () => {
   console.log('The application is listening on localhost:3000');
 });
